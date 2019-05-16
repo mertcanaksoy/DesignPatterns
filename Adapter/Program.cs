8f7eb42673aedca6c681939388fbfb4b80280799
Mert Aksoy <36101620+mertcanaksoy@users.noreply.github.com>
2019-05-16 10:52:04 +0300

    Adapter and Facade
    Adapter and Facade Patterns Added

---
+++ Adapter/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Adapter
+{
+    /*
+     * Farklı sistemlerin, kendi sistemimize entegre edilmesi durumunda kendi sistemimiz bozulmadan,
+     * farklı sistemin sanki kendi sistemimiz gibi davranmasını sağlayan desendir.
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new Log4NetAdapter());
+            //ProductManager productManager = new ProductManager(new MyLogger());
+            //farklı loglama çeşitleri eklenerek adapter deseniyle burada new'lenebilir
+            productManager.Save();
+            Console.ReadLine();
+        }
+    }
+
+    class ProductManager
+    {
+        private ILogger _logger;
+
+        public ProductManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            _logger.Log("Data Logged");
+            Console.WriteLine("Saved");
+        }
+    }
+
+    interface ILogger
+    {
+        void Log(string message);
+    }
+
+    class MyLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Logged, {0}",message);
+        }
+    }
+
+    class Log4Net //Dışarıdan müdahale edilemez bir class olduğunu varsayalım
+    {
+        public void LogMessage(string message)
+        {
+            Console.WriteLine("Logged with Log4Net, {0}", message);
+        }
+    }
+
+    class Log4NetAdapter : ILogger
+    {
+        public void Log(string message)
+        {
+            Log4Net log4Net = new Log4Net();
+            log4Net.LogMessage(message);
+        }
+    }
+}
---
+++ Facade/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomManager customManager = new CustomManager();
+            customManager.Save();
+            Console.ReadLine();
+        }
+
+        interface ILogging
+        {
+            void Log();
+        }
+
+        interface ICaching
+        {
+            void Cache();
+        }
+
+        interface IAuthorize
+        {
+            void CheckUser();
+        }
+
+        class Caching : ICaching
+        {
+            public void Cache()
+            {
+                Console.WriteLine("Cached");
+            }
+        }
+
+        class Logging : ILogging
+        {
+            public void Log()
+            {
+                Console.WriteLine("Logged");
+            }
+        }
+
+        class Authorize : IAuthorize
+        {
+            public void CheckUser()
+            {
+                Console.WriteLine("User Checked");
+            }
+        }
+
+        class CustomManager
+        {
+            private FacadeCrossCuttingConcerns _crossCuttingConcerns;
+
+            public CustomManager()
+            {
+                _crossCuttingConcerns = new FacadeCrossCuttingConcerns();
+            }
+
+            public void Save()
+            {
+                _crossCuttingConcerns.Logging.Log();
+                _crossCuttingConcerns.Authorize.CheckUser();
+                _crossCuttingConcerns.Caching.Cache();
+                Console.WriteLine("Saved");
+            }
+        }
+
+        class FacadeCrossCuttingConcerns
+        {
+            public ILogging Logging { get; set; }
+            public ICaching Caching { get; set; }
+            public IAuthorize Authorize { get; set; }
+
+            public FacadeCrossCuttingConcerns()
+            {
+                Logging = new Logging();
+                Caching = new Caching();
+                Authorize = new Authorize();
+                /*Yeni özellikler ve metodlar bu kısma eklenir, CustomManager adlı ana sınıfta
+                her yeni özellik için dependency injection yapma işlemine gerek kalmaz. Bu açıdan
+                Facade deseni oldukça faydalıdır. Bir nevi sadeleştirme işi görür. */
+            }
+        }
+    }
+}
