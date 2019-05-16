d58cafea6f989e49e5069b36890ad1bb8e05c93b
Mert Aksoy <36101620+mertcanaksoy@users.noreply.github.com>
2019-05-16 16:31:20 +0300

    Composite and Proxy Patterns Added
    Composite and Proxy Patterns Added

---
+++ Composite/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Composite Deseniyle Hiyerarşik Yapı Örneği");
+            Employee mert = new Employee {Name = "Mert"};
+            Employee damla = new Employee { Name = "Damla" };
+            mert.AddSubordinate(damla);
+
+            Employee gizem = new Employee { Name = "Gizem" };
+            mert.AddSubordinate(gizem);
+
+            Employee merve = new Employee {Name = "Merve"};
+            damla.AddSubordinate(merve);
+
+            Console.WriteLine("Mert");
+            foreach (Employee manager in mert)
+            {
+                Console.WriteLine("  {0}",manager.Name);
+                foreach (Employee employee in manager)
+                {
+                    Console.WriteLine("    {0}", employee.Name);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IPerson
+    {
+        string Name { get; set; }
+    }
+
+    class Employee : IPerson, IEnumerable<IPerson>
+    {
+        private List<IPerson> _subordinates = new List<IPerson>(); //Hiyerarşide alt kademe
+
+        public void AddSubordinate(IPerson person)
+        {
+            _subordinates.Add(person);
+        }
+
+        public void DeleteSubordinate(IPerson person)
+        {
+            _subordinates.Remove(person);
+        }
+
+        public IPerson GetSubordinate(int index)
+        {
+            return _subordinates[index];
+        }
+        public string Name { get; set; }
+        public IEnumerator<IPerson> GetEnumerator()
+        {
+            foreach (var subordinate in _subordinates)
+            {
+                yield return subordinate;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Cache'leme mantığı gibi düşünülebilir
+             Bir işlem ilk kez yapıldıktan sonra tekrar çağırılacağı zaman önceden yapılmış işlemi hızlıca
+             çağırma durumunda kullanılır.
+             */
+
+            CreditBase manager = new CreditManagerProxy();
+
+            Console.WriteLine(manager.Calculate());
+            Console.WriteLine(manager.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CreditBase
+    {
+        public abstract int Calculate();
+    }
+
+    class CreditManager : CreditBase
+    {
+        public override int Calculate()
+        {
+            int result = 3;
+            for (int i = 1; i < 5; i++)
+            {
+                result *= i;
+                Thread.Sleep(1000);
+            }
+
+            return result;
+        }
+    }
+
+    class CreditManagerProxy : CreditBase
+    {
+        private CreditManager _creditManager;
+        private int _cachedValue;
+        public override int Calculate()
+        {
+            if (_creditManager == null) //Eğer işlem ilk defa yapılıyorsa
+            {
+                _creditManager = new CreditManager();
+                _cachedValue = _creditManager.Calculate();
+            }
+            //İlk defa yapılmıyorsa 
+            return _cachedValue;
+        }
+    }
+}
